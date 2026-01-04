bba23bf7c4aaf9ad5b21e5742edcd659d1719110
Francis4402 <francism4402@gmail.com>
2026-01-04 12:14:18 +0600

    next config update

--- next.config.ts
+++ next.config.ts
@@ -3,27 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['http://161.248.189.254'],
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: '**',
-        port: '',
-        pathname: '/uploads/**',
-      },
-      {
-        protocol: 'https',
-        hostname: '**',
-        port: '',
-        pathname: '/uploads/**',
-      },
-    ],
-    localPatterns: [
-      {
-        pathname: "/uploads/**"
-      }
-    ]
-  }
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
